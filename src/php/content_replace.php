fedb644933bcc5e9f3992497a27182b9c51600e0
gackey.21 <gackey.21@gmail.com>
2019-03-26 17:25:19 +0900

    null　回避

--- src/php/content_replace.php
+++ src/php/content_replace.php
@@ -3,8 +3,10 @@
 function content_replace($the_content) {
 	$thx_cc_option = get_option('thx_cc_option');
 	$match_replace = json_decode( $thx_cc_option['content_replace_array'] , true ) ;
-	foreach ($match_replace as $match => $replace) {
-		$the_content = preg_replace($match, $replace, $the_content);
+	if ($match_replace) {
+		foreach ($match_replace as $match => $replace) {
+			$the_content = preg_replace($match, $replace, $the_content);
+		}
 	}
 	return $the_content;
 }
