29e43fca02406ed7d7f064532556b653b90c1652
gackey.21 <gackey.21@gmail.com>
2019-03-26 14:47:47 +0900

    ・optionを配列化 ・コールバックの共用化

--- src/php/menu.php
+++ src/php/menu.php
@@ -1,12 +1,19 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;
 //プラグイン名
 define('PLUG_IN_NAME', 'thx.jp/');
-//テーマ設定ページ用のURLクエリ
-// define('THEME_SETTINGS_PAFE', 'theme-settings');
-//トップレベルオリジナル設定名
-// define('SETTING_NAME_TOP', PLUG_IN_NAME.__( ' 設定', PLUG_IN_NAME ));
-
-// require_once('src/menu/menu.php');
+$thx_cc_option = get_option('thx_cc_option');
+if( !$thx_cc_option ) {
+	$thx_cc_option = array(
+		'remove_texturize' => 1,
+		'wao_space' => 1,
+		'wao_space_js_php' => 'jQuery',
+		'ruby' => 1,
+		'counted_heading' => 1,
+		'content_replace' => 1,
+		'content_replace_array' => '',
+	);
+	update_option( 'thx_cc_option', $thx_cc_option );
+}
 
 function thx_admin_menu() {
 	//設定のサブメニューに追加する
@@ -32,6 +39,7 @@ function thx_settings_plugin_options() {
 }
 // 管理画面の作成
 function thx_settings_init() {
+	register_setting('thx_settings-group', 'thx_cc_option');
 
 	//セクション設定
 	add_settings_section(
@@ -45,116 +53,122 @@ function thx_settings_init() {
 	add_settings_field(
 		'thx_remove_texturize',
 		'引用符の解除',
-		'thx_remove_texturize_callback',
-		'thx_settings','thx_settings_section'
+		'thx_single_checkbox_callback',
+		'thx_settings',
+		'thx_settings_section',
+		array(
+			'option_array_name' => 'remove_texturize',
+			'comment' => '引用符などの自動変換機能を解除する',
+			'add' => ''
+		)
 	);
-	register_setting('thx_settings-group', 'thx_remove_texturize');
 
 	//和欧間スペース
 	add_settings_field(
 		'thx_wao_space',
 		'和欧間スペース',
-		'thx_wao_space_callback',
+		'thx_single_checkbox_callback',
 		'thx_settings',
-		'thx_settings_section'
+		'thx_settings_section',
+		array(
+			'option_array_name' => 'wao_space',
+			'comment' => '和文と欧文の間にアキを設ける',
+			'add' => 'thx_wao_space_js_php_callback'
+		)
 	);
-	register_setting('thx_settings-group', 'thx_wao_space');
-
-	// add_settings_field(
-	// 	'thx_wao_space_js_php',
-	// 	'',
-	// 	'thx_wao_space_js_php_callback',
-	// 	'thx_settings',
-	// 	'thx_settings_section'
-	// );
-	register_setting('thx_settings-group', 'thx_wao_space_js_php');
 
 	//ルビ
 	add_settings_field(
 		'thx_ruby',
 		'行間の崩れないルビ',
-		'thx_ruby_callback',
+		'thx_single_checkbox_callback',
 		'thx_settings',
-		'thx_settings_section'
+		'thx_settings_section',
+		array(
+			'option_array_name' => 'ruby',
+			'comment' => '行間の崩れないルビを使用する',
+			'add' => ''
+		)
 	);
-	register_setting('thx_settings-group', 'thx_ruby');
 
 	//見出しのカウンター
 	add_settings_field(
 		'thx_counted_heading',
 		'見出しのカウンター',
-		'thx_counted_heading_callback',
+		'thx_single_checkbox_callback',
 		'thx_settings',
-		'thx_settings_section'
+		'thx_settings_section',
+		array(
+			'option_array_name' => 'counted_heading',
+			'comment' => '見出しにカウンターを付加する',
+			'add' => ''
+		)
 	);
-	register_setting('thx_settings-group', 'thx_counted_heading');
 
 	//コンテンツの置き換え
 	add_settings_field(
 		'thx_content_replace',
 		'コンテンツの置き換え（β版）',
-		'thx_content_replace_callback',
+		'thx_single_checkbox_callback',
 		'thx_settings',
-		'thx_settings_section'
+		'thx_settings_section',
+		array(
+			'option_array_name' => 'content_replace',
+			'comment' => '出力されるHTMLを正規表現で置換する',
+			'add' => 'thx_content_replace_array_callback'
+		)
 	);
-	register_setting('thx_settings-group', 'thx_content_replace');
-
-	add_settings_field(
-		'thx_content_replace_array',
-		'',
-		'thx_content_replace_array_callback',
-		'thx_settings',
-		'thx_settings_section'
-	);
-	register_setting('thx_settings-group', 'thx_content_replace_array');
 }
-
 function thx_settings_section_callback_function() {
 	echo '<p>各機能のON/OFFを切り替えます。</p>';
 }
 
 // コールバック
-function thx_remove_texturize_callback() {
-	echo '<p><input name="thx_remove_texturize" id="thx_remove_texturize" type="checkbox" value="1"';
-	checked( get_option( 'thx_remove_texturize', 1 ) );
-	echo ' />引用符などの自動変換機能を解除する</p>';
-}
-function thx_wao_space_callback() {
-	echo '<p><input name="thx_wao_space" id="thx_wao_space" type="checkbox" value="1"';
-	checked( get_option( 'thx_wao_space' ), 1 );
-	echo ' />和文と欧文の間にアキを設ける</p>';
-	thx_wao_space_js_php_callback();
+function thx_single_checkbox_callback($args) {
+	$thx_cc_option = get_option('thx_cc_option');
+	$option_name = $args['option_array_name'];
+	$name_id = 'name="thx_cc_option['.$option_name.']" id="thx_'.$option_name.'"';
+	?>
+	<p>
+		<input type="hidden" <?=$name_id?> value="0" />
+		<input type="checkbox" <?=$name_id?> value="1"
+		<?php checked( $thx_cc_option[$option_name], 1 ); ?>
+		/><?=$args['comment']?>
+	</p>
+	<?php
+	if ($args['add']) {
+		call_user_func($args['add']);
+	}
 }
 function thx_wao_space_js_php_callback() {
-	echo '<p>　<input name="thx_wao_space_js_php" id="thx_wao_space_js_php" type="radio" value="jQuery"';
-	checked( get_option( 'thx_wao_space_js_php' ), 'jQuery' );
-	echo ' />jQuery</p>';
-	echo '<p>　<input name="thx_wao_space_js_php" id="thx_wao_space_js_php" type="radio" value="php"';
-	checked( get_option( 'thx_wao_space_js_php' ), 'php' );
-	echo ' />php</p>';
-}
-function thx_ruby_callback() {
-	echo '<p><input name="thx_ruby" id="thx_ruby" type="checkbox" value="1"';
-	checked( get_option( 'thx_ruby', 1 ), 1 );
-	echo ' />行間の崩れないルビを使用する</p>';
-}
-function thx_counted_heading_callback() {
-	echo '<p><input name="thx_counted_heading" id="thx_counted_heading" type="checkbox" value="1"';
-	checked( get_option( 'thx_counted_heading', 1 ), 1 );
-	echo ' />見出しにカウンターを付加する</p>';
-}
-function thx_content_replace_callback() {
-	echo '<p><input name="thx_content_replace" id="thx_content_replace" type="checkbox" value="1"';
-	checked( get_option( 'thx_content_replace', 1 ), 1 );
-	echo ' />出力されるHTMLを正規表現で置換する</p>';
+	$thx_cc_option = get_option('thx_cc_option');
+	?>
+	<p>
+		　<input type="radio" name="thx_cc_option[wao_space_js_php]" id="thx_wao_space_js_php" value="jQuery"
+		<?php checked( $thx_cc_option['wao_space_js_php'], 'jQuery' ); ?>
+		/>
+		jQuery
+	</p>
+	<p>
+		　<input type="radio" name="thx_cc_option[wao_space_js_php]" id="thx_wao_space_js_php" value="php"
+		<?php checked( $thx_cc_option['wao_space_js_php'], 'php' ); ?>
+		/>
+		php
+	</p>
+	<?php
 }
 function thx_content_replace_array_callback() {
-	echo '<textarea
+	$thx_cc_option = get_option('thx_cc_option');
+	?>
+	<textarea
+	name="thx_cc_option[content_replace_array]"
 	id="thx_content_replace_array"
-	name="thx_content_replace_array"
 	cols="80"
 	rows="4"
-	placeholder="{&quot;正規表現&quot;:&quot;置換文字&quot;,･･･}">'
-	.get_option( 'thx_content_replace_array' ).
-	'</textarea>';
+	placeholder="{&quot;正規表現&quot;:&quot;置換文字&quot;,･･･}">
+<?php echo $thx_cc_option['content_replace_array']; ?>
+	</textarea>
+	<?php
+	// global $thx_cc_setting;
+	// var_dump($thx_cc_setting);
 }
