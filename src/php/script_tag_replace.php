7d18dd12d89b0e42c1a278b83f6af83711b46174
gackey.21 <gackey.21@gmail.com>
2019-03-29 08:43:02 +0900

    null処理　修正

--- src/php/script_tag_replace.php
+++ src/php/script_tag_replace.php
@@ -2,12 +2,6 @@
 //jQueryの非同期読み込み
 function replace_script_tag ( $tag ) {
 	$thx_cc_option = get_option('thx_cc_option');
-	$exclusion = str_replace(
-		array("\r\n", "\r", "\n"),
-		"\n",
-		$thx_cc_option['js_async_defer_array']
-	);
-	$exclusion = explode("\n", $exclusion);
 	if (!preg_match('/\b(defer|async)\b/', $tag)) {
 		$tag = str_replace(
 			"type='text/javascript'",
@@ -15,7 +9,14 @@ function replace_script_tag ( $tag ) {
 			$tag
 		);
 	}
+	$exclusion = str_replace(
+		array("\r\n", "\r", "\n"),
+		"\n",
+		$thx_cc_option['js_async_defer_array']
+	);
 	if ($exclusion) {
+		// var_dump($exclusion);
+		$exclusion = explode("\n", $exclusion);
 		foreach ($exclusion as $value) {
 			if (strpos($tag,$value) !== false) {
 				$tag = str_replace(
