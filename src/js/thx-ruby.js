71bb7bd6d3701570dac08da54327415603803118
gackey.21 <gackey.21@gmail.com>
2019-05-15 10:03:13 +0900

    ruby inline-block化＋width付与方式から inlineのまま四分スペース付与方式に変更

--- src/js/thx-ruby.js
+++ src/js/thx-ruby.js
@@ -20,16 +20,22 @@ jQuery(window).on('load', function() {
 			yomigana_span += ' thx_yomi_long';
 		}
 
-		var ts = (lh - fz) / 2 - (fz / 2);
+		var ts = - (fz / 2);
 		yomigana_span += '" style="top: ' + ts + 'px">';
 
+		var q_space_length = (yomigana_length - ow) / fz * 2;
+		var span_q_space = '';
 		if (yomigana_length > ow) {
-			jQuery(this).css('width', yomigana_length);
+			span_q_space = '<span class="thx_quo_spc">';
+			for (var i = 0; i < q_space_length; i++) {
+				span_q_space += '&#x2005;'
+			}
+			span_q_space += '</span>'
 		}
 		for (var i = 0; i < yomigana.length; i++) {
 			yomigana_span += '<span>' + yomigana.substr(i, 1) + '</span>';
 		}
 		yomigana_span += '</div>';
-		jQuery(this).html(ruby_html + yomigana_span);
+		jQuery(this).html(span_q_space + ruby_html + span_q_space + yomigana_span);
 	});
 });
